16b88ba62eff812c6c5b036666ad9ebe5d98ab77
Laura Villalobos <lauravillalobossilva@gmail.com>
2025-12-30 21:30:05 -0500

    agrega metodologia BEM

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
-    <nav className="navbar-component">
-      <Link to="/home">Inicio</Link>
-      <Link to="/categorias">Categorias</Link>
-      <Link to="/libros">Libros</Link>
-      <Link to="/components">Component</Link>
+    <nav className="navbar">
+      <Link to="/home" className="navbar__link">Inicio</Link>
+      <Link to="/categorias" className="navbar__link">Categorias</Link>
+      <Link to="/libros" className="navbar__link">Libros</Link>
+      <Link to="/components" className="navbar__link">Component</Link>
     </nav>
   );
 }
